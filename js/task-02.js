afddb37be26f98267bcad825d0a2b9da0c0808c8
Mark-Y <yakushyn.mark@gmail.com>
2021-07-22 00:13:48 +0300

    Закончил второе задание

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,30 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+
+
+const ingredientsListRef = document.querySelector('#ingredients');
+
+const addIngredients = (products) => {
+  
+  const newIngredients = products.map(product => {
+
+    const newItem = document.createElement("li");
+
+    newItem.classList.add("ingredients-item");
+
+    newItem.textContent = product;
+  
+    return newItem;
+
+  })
+
+  ingredientsListRef.append(...newIngredients);
+
+};
+
+addIngredients(ingredients);
+
+
+
