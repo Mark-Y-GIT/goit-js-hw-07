81e79fe9bad5eae60e8898b5f960e378a515efe3
Mark-Y <yakushyn.mark@gmail.com>
2021-07-21 22:03:24 +0300

    первое задание

---
+++ js/task-01.js
@@ -0,0 +1,25 @@
+const categoriesRef = document.querySelectorAll(".item");
+
+
+const categoryList = function () {
+    
+    console.log(`В списке ${categoriesRef.length} категории.`);
+};
+
+
+
+const categoryAndQuantity = () => {
+    
+categoriesRef.forEach(element => {
+    
+    console.log(`Категория: ${element.querySelector("h2").textContent}`);
+
+    console.log(`Количество элементов: ${ element.querySelector("ul").children.length}`);
+    
+});
+
+}
+
+categoryList()
+
+categoryAndQuantity()
